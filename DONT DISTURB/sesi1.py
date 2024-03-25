e7acd7d55e3a807529161da5f3db2a06b5ef0bc9
Dwipayana2201010152 <127906331+Dwipayana2201010152@users.noreply.github.com>
2024-03-25 11:45:46 +0800

    Add files via upload

---
+++ DONT DISTURB/sesi1.py
@@ -0,0 +1,52 @@
+
+print("Didokpayana")
+
+x = 5.5
+y = 7.2
+hasil = x * y
+print("Hasil dari operasi tersebut adalah : ", hasil)
+
+z = 6.5
+w = "Bahasa Ular"
+coba = z + x
+print(coba)
+
+#Tipe data boolean
+trueORFalse = trueORFalse
+print(trueORFalse)
+
+#Tipe data Array
+ar = ['Blue','green','red']
+print(ar[0])
+
+#tipe data dictionary
+dic = {'nama buah': 'durian',
+        'nama ilmiah': 'didokpayana'
+        }
+print(dic['nama buah'])
+
+#Pernyataan Kondisional
+if 5 > 2:
+    print("Lima lebih besar lebih dua")
+
+
+x = 4
+y = (8-x)
+if x == y:
+    print('X dan Y sama')
+else:
+    print("Bedah")
+
+
+#Mendefinisikan Fungsi
+def ini_fungsi(siapaNamanya):
+    print(siapaNamanya + "Belajar PCD")
+
+#Memanggil fungsi
+ini_fungsi("Didokpayana ")
+
+import cv2
+import datatime as dt
+
+x = dt.datetime.mw()
+print(x)
